d6f16bd1b7fc841932ce473a1f6e9ba6541aee8c
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-03-11 16:50:19 +0800

    style: search bar with search button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { RiMovie2AiFill } from "react-icons/ri";
 import { IoMdMoon } from "react-icons/io";
 import { FaHome } from "react-icons/fa";
 import { FaInfoCircle } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 import Movie from "./components/Movie";
 export default function Home() {
   return (
@@ -34,6 +35,18 @@ export default function Home() {
         <div className="space-x-5 text-md font-semibold text-white">
           <Link href="/trending">Trending</Link>
           <Link href="/top-rated">Top Rated</Link>
+        </div>
+      </div>
+      <div className="my-10">
+        <div className="flex gap-3">
+          <input
+            type="text"
+            className="py-1 px-2 border border-slate-500 rounded-md outline-none bg-gray-300 w-full text-xl placeholder:text-gray-400"
+            placeholder="Search Movie Title..."
+          />
+          <button className="bg-red-600 px-3 py-1 text-xl rounded-md text-white">
+            <FaSearch />
+          </button>
         </div>
       </div>
       <div className="grid gap-5 grid-cols-12">
